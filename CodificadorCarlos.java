8f3d3a9a888162dc9305aea30ccce657840ad58c
CaduESG <cadu_sgriebeler@hotmail.com>
2025-08-06 18:10:38 +0000

    Codificador Carlos Finalizado

--- CodificadorCarlos.java
+++ CodificadorCarlos.java
@@ -1,14 +1,25 @@
-// Carlos Eduardo Griebeler
+import java.time.LocalDate;
+
 public class CodificadorCarlos implements Codificador {
     public int getNivelSeguranca(){
         return 10;
     }
 
+    public String getNome() {
+        return "Codificador Carlos";
+    }
+
+    public LocalDate getDataCriacao() {
+        return LocalDate.of(2025, 03, 13);
+    }
+
     public String codifica(String str) {
         StringBuilder encoded = new StringBuilder();
 
+        int soma = 1;
         for (char c : str.toCharArray()) {
-            encoded.append((char) (c + 1));
+            encoded.append((char) (c + soma));
+            soma++;
         }
 
         return encoded.toString();
@@ -17,8 +28,10 @@ public class CodificadorCarlos implements Codificador {
     public String decodifica(String str) {
         StringBuilder encoded = new StringBuilder();
         
+        int soma = 1;
         for (char c : str.toCharArray()) {
-            encoded.append((char) (c - 1));
+            encoded.append((char) (c - soma));
+            soma++;
         }
         
         return encoded.toString();
