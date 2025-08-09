2f3df42f3cf81a2bb0c52dc2a5abc1566b7068f8
CaduESG <cadu_sgriebeler@hotmail.com>
2025-08-09 15:22:55 +0000

    Embelezando a app

--- App.java
+++ App.java
@@ -1,22 +1,45 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Codificador cod = new CodificadorPedro();
-        CodificadorMathz cod2 = new CodificadorMathz();
+        Codificador pedro = new CodificadorPedro();
+        Codificador matheus = new CodificadorMathz();
+        Codificador carlos = new CodificadorCarlos();
+        String texto = "aaaaa", codificado, decodificado;
 
-        System.out.println(cod2.codifica("Teste"));
-        System.out.println(cod2.decodifica("Qbpqb"));
+        System.out.println("Testes para funcionamento dos codificadores.");        
+
+        // Codificador Pedro
+        System.out.println("Codificador: " + pedro.getNome());
+        System.out.println("Versao: " + pedro.getDataCriacao());
+        System.out.println("Nivel de segurança: " + pedro.getNivelSeguranca());
+        System.out.println("Texto original: " + texto);
+        codificado = pedro.codifica(texto);
+        System.out.println("Texto codificado: " + codificado);
+        decodificado = pedro.decodifica(codificado);
+        System.out.println("Texto decodificado: " + decodificado);
+
+        System.out.println("\n==================================\n");
         
-
-        System.out.println("Codificador: "+cod.getNome());
-        System.out.println("Versao: "+cod.getDataCriacao());
-        System.out.println("Nivel de segurança: "+cod.getNivelSeguranca());
+        // Codificador Matheus
+        System.out.println("Codificador: " + matheus.getNome());
+        System.out.println("Versao: " + matheus.getDataCriacao());
+        System.out.println("Nivel de segurança: " + matheus.getNivelSeguranca());
+        System.out.println("Texto original: " + texto);
+        codificado = matheus.codifica(texto);
+        System.out.println("Texto codificado: " + codificado);
+        decodificado = matheus.decodifica(codificado);
+        System.out.println("Texto decodificado: " + decodificado);
         
-        String texto = "zzz";
-        String codificado = cod.codifica(texto);
-        String decodificado = cod.decodifica(codificado);
+        System.out.println("\n==================================\n");
 
-        System.out.println("Texto original: "+texto);
-        System.out.println("Texto codificado: "+codificado);
-        System.out.println("Texto decodificado: "+decodificado);
+
+        // Codificador Carlos
+        System.out.println("Codificador: " + carlos.getNome());
+        System.out.println("Versao: " + carlos.getDataCriacao());
+        System.out.println("Nivel de segurança: " + carlos.getNivelSeguranca());
+        System.out.println("Texto original: " + texto);
+        codificado = carlos.codifica(texto);
+        System.out.println("Texto codificado: " + codificado);
+        decodificado = carlos.decodifica(codificado);
+        System.out.println("Texto decodificado: " + decodificado);
     }
 }
